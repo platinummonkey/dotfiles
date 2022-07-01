9af80caf3fb51db1e5863b9e9547a431a12c246a
Cody Lee <cody.lee@datadoghq.com>
2022-07-01 09:32:36 -0500

    fix functinos

--- workspace/customizations.sh
+++ workspace/customizations.sh
@@ -2,11 +2,11 @@ export EDITOR="vim"
       
 alias replaceNewlines="sed ':a;N;$!ba;s/\n/\\n/g'"
       
-function cdd{
+function cdd() {
   subdir="${1}"
   cd $DATADOG_ROOT/$subdir
 }
 
-find_merge_for() {
+function find_merge_for() {
   git log $1..prod --ancestry-path --merges --reverse
 }
